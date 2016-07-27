4aca3293c42016e213d093da89c507750a9f092e
Leif_Chen <silver78821@gmail.com>
2016-07-27 03:38:51 +0000

    改密不可用其他帳號bug修復

--- controllers/changepassword_check_con.php
+++ controllers/changepassword_check_con.php
@@ -14,18 +14,25 @@ class changepassword_check_con extends Controller{
             if (isset($_POST["btnOK"])) {
                 $sUserName = $_POST["txtUserName"];
                 $sUserPassword = $_POST["txtPassword"];
-            
-                // 驗證方法同登入驗證
-                $member = $this->model("login_mod");
-                $check = $member->login($sUserName,$sUserPassword);
                 
-                if($check != 1) {
+                if($_COOKIE["userName"] != $sUserName) {
                     echo "<script language='JavaScript'>";
-                    echo "alert('帳號或密碼輸入有誤');";
+                    echo "alert('您輸入的帳號不是你的呦');";
                     echo "</script>";
                 }
                 else {
-                    header("Location: ../changepassword_change/changepassword_change");
+                    // 驗證方法同登入驗證
+                    $member = $this->model("login_mod");
+                    $check = $member->login($sUserName,$sUserPassword);
+                    
+                    if($check != 1) {
+                        echo "<script language='JavaScript'>";
+                        echo "alert('密碼輸入有誤');";
+                        echo "</script>";
+                    }
+                    else {
+                        header("Location: ../changepassword_change/changepassword_change");
+                    }
                 }
             }
         }
